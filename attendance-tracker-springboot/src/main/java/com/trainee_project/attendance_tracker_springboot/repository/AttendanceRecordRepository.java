2efb3c60079c8b29ea289b90a51dc21e6c66ed4c
zarn_chalamet <6531503195@lamduan.mfu.ac.th>
2025-09-15 18:06:05 +0700

    create repos

---
+++ attendance-tracker-springboot/src/main/java/com/trainee_project/attendance_tracker_springboot/repository/AttendanceRecordRepository.java
@@ -0,0 +1,9 @@
+package com.trainee_project.attendance_tracker_springboot.repository;
+
+import com.trainee_project.attendance_tracker_springboot.model.AttendanceRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRecordRepository extends JpaRepository<AttendanceRecord, String> {
+}
---
+++ attendance-tracker-springboot/src/main/java/com/trainee_project/attendance_tracker_springboot/repository/OfficeLocationRepository.java
@@ -0,0 +1,9 @@
+package com.trainee_project.attendance_tracker_springboot.repository;
+
+import com.trainee_project.attendance_tracker_springboot.model.OfficeLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficeLocationRepository extends JpaRepository<OfficeLocation, String> {
+}
